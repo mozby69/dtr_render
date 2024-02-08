37bd3083c5fde62845678b2b08d233185121363f
john <johnxyrylpedrosa31@gmail.com>
2024-02-08 14:21:19 +0800

    3

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+admin.site.register(models.DailyRecord)
+admin.site.register(models.temporay)
+admin.site.register(models.Branches)
+admin.site.register(models.Employee)
+admin.site.register(models.RequestForm)
+admin.site.register(models.AttendanceCount)
+admin.site.register(models.EmployeeStatus)
--- myproject/settings.py
+++ myproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-k+2o1e6n3%f(yy&9&p9opd_)@#w714p!d^nsu5vh4!iie2j8!)'
 
-
-#DEBUG = os.environ.get('DEBUG', 'True') == "True"
-DEBUG = False
+DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','webapp2-akkf.onrender.com']
 
@@ -77,24 +76,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if not DEBUG:
-    DATABASE = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# if not DEBUG:
+#     DATABASE = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     }
+
+# else:
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
-else:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'saitama123',
-            'HOST':'db',
-            'PORT': '5432',
-        }
-    }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -149,3 +143,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
