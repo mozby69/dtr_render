e8d7288d1ba8d86a530c5e9ed0bf10bbcaa5158a
john <johnxyrylpedrosa31@gmail.com>
2024-02-15 13:08:32 +0800

    second update

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','webapp2-akkf.onrender.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','dtr-qrscanner.onrender.com']
 
 
 # Application definition
